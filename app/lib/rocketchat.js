d33dafb9d70697fa5a210fea8174191fd7491a73
Daniel Maike <danmke@hotmail.com>
2020-11-26 16:33:35 -0300

    refactor: distribute responsible of invites and rooms

--- app/lib/rocketchat.js
+++ app/lib/rocketchat.js
@@ -1397,19 +1397,6 @@ const RocketChat = {
 			return room.uids?.length + room.usernames?.join();
 		}
 		return room.prid ? room.fname : room.name;
-	},
-
-	findOrCreateInvite({ rid, days, maxUses }) {
-		// RC 2.4.0
-		return this.post('findOrCreateInvite', { rid, days, maxUses });
-	},
-	validateInviteToken(token) {
-		// RC 2.4.0
-		return this.post('validateInviteToken', { token });
-	},
-	useInviteToken(token) {
-		// RC 2.4.0
-		return this.post('useInviteToken', { token });
 	}
 };
 
