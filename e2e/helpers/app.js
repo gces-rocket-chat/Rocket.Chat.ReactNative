599c1a2986a196306966c7f81cb2455ad90bdaa8
Daniel Maike <danmke@hotmail.com>
2020-11-26 14:31:37 -0300

    refactor: applying clean code to avoid duplication
    Signed-off-by: Daniel Maike <danmke@hotmail.com>

--- e2e/helpers/app.js
+++ e2e/helpers/app.js
@@ -3,44 +3,48 @@ const {
 } = require('detox');
 const data = require('../data');
 
+async function waitForToBeVisible(element, timeout) {
+    await waitFor(element).toBeVisible().withTimeout(timeout);
+}
+
 async function navigateToWorkspace() {
-    await waitFor(element(by.id('onboarding-view'))).toBeVisible().withTimeout(10000);
+    await waitForToBeVisible(element(by.id('onboarding-view')), 10000);
 	await element(by.id('join-workspace')).tap();
-	await waitFor(element(by.id('new-server-view'))).toBeVisible().withTimeout(60000);
+	await waitForToBeVisible(element(by.id('new-server-view')), 60000);
 	await element(by.id('new-server-view-input')).replaceText(data.server);
 	await element(by.id('new-server-view-button')).tap();
-	await waitFor(element(by.id('workspace-view'))).toBeVisible().withTimeout(60000);
+	await waitForToBeVisible(element(by.id('workspace-view')), 60000);
 	await expect(element(by.id('workspace-view'))).toBeVisible();
 }
 
 async function navigateToLogin() {
-    await waitFor(element(by.id('onboarding-view'))).toBeVisible().withTimeout(20000);
+    await waitForToBeVisible(element(by.id('onboarding-view')), 20000);
     await navigateToWorkspace();
-	await element(by.id('workspace-view-login')).tap();
-    await waitFor(element(by.id('login-view'))).toBeVisible().withTimeout(2000);
+    await element(by.id('workspace-view-login')).tap();
+    await waitForToBeVisible(element(by.id('login-view')), 2000);
     await expect(element(by.id('login-view'))).toBeVisible();
 }
 
 async function navigateToRegister() {
-    await waitFor(element(by.id('onboarding-view'))).toBeVisible().withTimeout(20000);
+    await waitForToBeVisible(element(by.id('onboarding-view')), 20000);
     await navigateToWorkspace();
-	await element(by.id('workspace-view-register')).tap();
-    await waitFor(element(by.id('register-view'))).toBeVisible().withTimeout(2000);
+    await element(by.id('workspace-view-register')).tap();
+    await waitForToBeVisible(element(by.id('register-view')), 2000);
     await expect(element(by.id('register-view'))).toBeVisible();
 }
 
 async function login(username, password) {
-    await waitFor(element(by.id('login-view'))).toBeVisible().withTimeout(2000);
+    await waitForToBeVisible(element(by.id('login-view')), 2000);
     await element(by.id('login-view-email')).replaceText(username);
     await element(by.id('login-view-password')).replaceText(password);
     await element(by.id('login-view-submit')).tap();
-    await waitFor(element(by.id('rooms-list-view'))).toBeVisible().withTimeout(10000);
+    await waitForToBeVisible(element(by.id('rooms-list-view')), 10000);
 }
 
 async function logout() {
     await element(by.id('rooms-list-view-sidebar')).tap();
-    await waitFor(element(by.id('sidebar-view'))).toBeVisible().withTimeout(2000);
-	await waitFor(element(by.id('sidebar-settings'))).toBeVisible().withTimeout(2000);
+    await waitForToBeVisible(element(by.id('sidebar-view')), 2000);
+    await waitForToBeVisible(element(by.id('sidebar-settings')), 2000);
     await element(by.id('sidebar-settings')).tap();
     await waitFor(element(by.id('settings-view'))).toBeVisible().withTimeout(2000);
     await element(by.type('UIScrollView')).atIndex(1).scrollTo('bottom');
@@ -49,7 +53,7 @@ async function logout() {
     await waitFor(element(by.text(logoutAlertMessage)).atIndex(0)).toExist().withTimeout(10000);
     await expect(element(by.text(logoutAlertMessage)).atIndex(0)).toExist();
     await element(by.text('Logout')).tap();
-    await waitFor(element(by.id('onboarding-view'))).toBeVisible().withTimeout(10000);
+    await waitForToBeVisible(element(by.id('onboarding-view')), 10000);
     await expect(element(by.id('onboarding-view'))).toBeVisible();
 }
 
@@ -64,7 +68,7 @@ async function mockMessage(message) {
 
 async function starMessage(message){
     const messageLabel = `${ data.random }${ message }`
-    await waitFor(element(by.label(messageLabel))).toBeVisible().withTimeout(5000);
+    await waitForToBeVisible(element(by.label(messageLabel)), 5000);
     await element(by.label(messageLabel)).atIndex(0).longPress();
     await expect(element(by.id('action-sheet'))).toExist();
     await expect(element(by.id('action-sheet-handle'))).toBeVisible();
